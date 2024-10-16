2b490fad9484e23b3dd7c920aec2ba9613c3a191
hassan khodabakhshi <80225351+hassankhodabakhshi@users.noreply.github.com>
2024-10-16 11:00:28 +0330

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -6,11 +6,11 @@ import { SignJWT, jwtVerify } from 'jose';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'a8d5eee7-f0af-453a-a735-a210f5b2c76a';
 let trojanPassword = `bpb-trojan`;
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
-const proxyIPs= ['bpb.yousef.isegaro.com'];
+const proxyIPs= ['speed.syseleven.net'];
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
